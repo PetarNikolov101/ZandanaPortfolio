ec023866531654e7d2af27374b0a0e9bfa207c2c
PetarNikolov101 <petarnikolov101@gmail.com>
2024-09-06 23:34:58 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Navbar from "./Navbar";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <Navbar/>
+   <App></App>
   </React.StrictMode>
 );
 
